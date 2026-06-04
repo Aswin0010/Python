6f47d72db3d5bbe91785a5b5d1db9ea1e986e6e0
Aswin Rai <raiaswin650@gmail.com>
2026-06-04 21:33:53 +0530

    Delete list directory

--- list/practice_list_1.py
+++
@@ -1,6 +0,0 @@
-n=int(input("Enter the size of list : "))
-l=[ ]
-for i in range (n):
-    a=input("Enter the number :")
-    l.append(a)
-print(l)    
--- list/practice_list_6.py
+++
@@ -1,9 +0,0 @@
-n=int(input("Enter the size of list : "))
-l=[ ]
-sum=0
-for i in range (n):
-    a=int(input("Enter the number :"))
-    l.append(a)
-for j in l:
-    sum=sum+j
-print("sum of elements:",sum)
